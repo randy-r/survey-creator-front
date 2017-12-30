dcad9b2db9d9ce1953ce92bf5ca0a6164252dddd
randy-r <31799713+randy-r@users.noreply.github.com>
2017-12-30 08:33:44 +0200

    react-md (#1)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { NavigationDrawer, Button, FontIcon } from 'react-md';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -11,18 +13,35 @@ class App extends Component {
       })
       .then(foos => console.log('Got foos: ', foos));
   }
-  
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <NavigationDrawer
+        drawerTitle="react-md with CRA"
+        toolbarTitle="Welcome to react-md"
+      >
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
+          </p>
+        </div>
+        <i className="material-icons">done</i>
+        <Button raised primary iconEl={<FontIcon>done</FontIcon>} >Spock</Button>
+        <Button raised secondary iconEl={<FontIcon>done</FontIcon>} >Spock</Button>
+        <Button flat secondary iconEl={<FontIcon>done</FontIcon>} >Spock</Button>
+
+        <h4 className="md-cell md-cell--12">Font icons from Material Icons</h4>
+        <FontIcon>home</FontIcon>
+        <FontIcon>favorite</FontIcon>
+        <h4 className="md-cell md-cell--12">Font icons from FontAwesome</h4>
+        <FontIcon iconClassName="fa fa-star-o" />
+        <FontIcon iconClassName="fa fa-book" />
+        {/* SVGIcons element can be used after the setup : https://react-md.mlaursen.com/components/svg-icons */}
+      </NavigationDrawer>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import WebFontLoader from 'webfontloader';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+  custom: {
+		families: ['FontAwesome'],
+		urls: ['https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css'],
+	},
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
